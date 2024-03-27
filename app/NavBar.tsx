e751a8917baf9a61e11cff4c69c809bfaed5c7b4
Dinujaya Sandaruwan <info.dinujaya@gmail.com>
2024-03-27 13:52:11 +0530

    5- Accessing Sessions on the Client

---
+++ app/NavBar.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+import Link from "next/link";
+import React from "react";
+
+const NavBar = () => {
+  const { status, data: session } = useSession();
+
+  return (
+    <nav className="bg-gray-800 text-white p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <a href="/" className="text-lg font-semibold">
+          BrandName
+        </a>
+        <div>
+          <Link
+            href="/users"
+            className="px-3 py-2 rounded hover:bg-gray-700 transition-colors"
+          >
+            Users
+          </Link>
+          <Link
+            href=""
+            className="px-3 py-2 rounded hover:bg-gray-700 transition-colors"
+          >
+            Services
+          </Link>
+          {status === "loading" && <span>Loading...</span>}
+          {status === "unauthenticated" && (
+            <Link
+              href="/api/auth/signin"
+              className="px-3 py-2 rounded hover:bg-gray-700 transition-colors"
+            >
+              Sign In
+            </Link>
+          )}
+          {status === "authenticated" && <span>{session?.user!.name}</span>}
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
